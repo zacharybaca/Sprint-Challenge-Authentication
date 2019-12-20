bc8494c7f460fe395b082191acda0eaac5f20b8f
zacharybaca <baca.zach@icloud.com>
2019-12-20 15:08:46 -0600

    updated tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -2,54 +2,94 @@ const request = require('supertest');
 
 const server = require('../api/server.js');
 
+const Users = require('../database/dbConfig.js');
+
+let token = '';
+
 describe('server.js', function() {
+  describe('server', function() {
+    describe('POST truncate', function() {
+      beforeEach(async () => {
+        await Users('users').truncate();
+      });
+    });
+    it('should return 404', () => {
+      const newUser = { username: 'chance', password: 'testing' };
+      return request(server)
+        .post('/register')
+        .send(newUser)
+        .then(res => {
+          expect(res.status).toBe(404);
+        });
+    });
+  });
   describe('environment', function() {
     it('should set environment to testing', function() {
       expect(process.env.DB_ENV).toBe('testing');
     });
   });
-
-  describe('POST /', function() {
-    it('should return a 201', function() {
-      // spin up the server
+  describe('POST ', function() {
+    it('should return 404', function() {
       return request(server)
         .post('/register')
+        .send({ username: 'marco', password: 'polo' })
         .then(res => {
-          expect(res.status).toBe(201);
-        });
-      // make GET request to /
-      // look at the http status code for the response
-    });
-
-    it.skip('auth example', function() {
-      return request(server)
-        .post('/login')
-        .send({ username: 'me', password: 'pass' })
-        .then(res => {
-          const token = res.body.token;
-
-          return request(server)
-            .get('/')
-            .set('Authorization', token)
-            .then(res => {
-              expect(res.status).toBe(200);
-              expect(Array.isArray(res.body)).toBe(true);
-            });
+          expect(res.status).toBe(404);
         });
     });
   });
-  describe('POST /users', function() {
-    it('responds with json', function(done) {
-      request(server)
-        .post('/register')
+  describe('POST', function() {
+    it('should return 404', function() {
+      return request(server)
+        .post('/login')
         .send({ username: 'sunny', password: 'test' })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function(err, res) {
-          if (err) return done(err);
-          done();
+        .then(res => {
+          expect(res.status).toBe(404);
         });
+    });
+  });
+
+  describe('POST', function() {
+    it('should return a 401', function() {
+      return request(server)
+        .post('/api/auth/login')
+        .send({ username: 'jellybean', password: 'bean' })
+        .then(res => {
+          expect(res.status).toBe(401);
+        });
+    });
+  });
+  describe('POST /register', () => {
+    it('should return 500', () => {
+      return request(server)
+        .post('/api/auth/register')
+        .send({ username: 'aaa1', password: 'aaa' })
+        .then(res => {
+          expect(res.status).toBe(500);
+        });
+    });
+  });
+  describe('dad joke server', () => {
+    it('should return 401 no creds', () => {
+      return request(server)
+        .get('/api/jokes')
+        .then(res => {
+          expect(res.status).toBe(401);
+        });
+    });
+    it('should return JSON data', () => {
+      return request(server)
+        .get('/api/jokes')
+        .then(res => {
+          expect(res.type).toMatch(/json/i);
+        });
+    });
+  });
+  describe('jokes', () => {
+    it('Return json', () => {
+      return request(server)
+        .get('/api/jokes')
+        .expect('Content-Type', /json/);
     });
   });
 });
